87dc0009929d1d703fe850134e41455684b5ab57
David Boschwitz <david@boschwitz.me>
2016-11-13 13:52:43 -0600

    updated controller to include filter

--- frontend/controller.js
+++ frontend/controller.js
@@ -10,8 +10,8 @@ function httpService($http) {
 var endpoint = 'http://vps.boschwitz.me:3333/';
 
 angular.module('quizApp', [])
-// h/t http://ecommerce.shopify.com/c/ecommerce-design/t/ordinal-number-in-javascript-1st-2nd-3rd-4th-29259
-.filter('ordinal', function() {
+    // h/t http://ecommerce.shopify.com/c/ecommerce-design/t/ordinal-number-in-javascript-1st-2nd-3rd-4th-29259
+    .filter('ordinal', function() {
         return function(input) {
             var s = ["th", "st", "nd", "rd"],
                 v = input % 100;
